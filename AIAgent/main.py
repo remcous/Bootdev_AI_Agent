15df2cfa4e080f7875666d856c325465dc51e157
remcous <67499364+remcous@users.noreply.github.com>
2026-04-06 22:12:37 -0700

    Ch 1 Lesson 6 completed: * added user role and messages list * refactored to add an explicit main function and separate out generate_content to easy reuse

--- AIAgent/main.py
+++ AIAgent/main.py
@@ -1,30 +1,35 @@
 import os
-from dotenv import load_dotenv
-from google import genai
 import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-if api_key is None:
-    raise RuntimeError("GEMINI_API_KEY environment variable not found")
+def main():
+    parser = argparse.ArgumentParser(description="AIAgent")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+    
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-parser = argparse.ArgumentParser(description="AIAgent")
-parser.add_argument("user_prompt", type=str, help="User Prompt")
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages)
 
-args = parser.parse_args()
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages
+    )
+    if response.usage_metadata == None:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
-client = genai.Client(api_key=api_key)
-
-print(f"User Prompt: {args.user_prompt}")
-
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents=args.user_prompt
-)
-
-if response.usage_metadata == None:
-    raise RuntimeError("API request failed, recieved response with no usage metadata")
-
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-print(f"Response:\n{response.text}")
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f"Response:\n{response.text}")
+    
+if __name__ == "__main__":
+    main()
