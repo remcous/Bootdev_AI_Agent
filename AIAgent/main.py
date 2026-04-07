6bb11638b9da36880001421451af37adfa1b292e
remcous <67499364+remcous@users.noreply.github.com>
2026-04-06 22:21:17 -0700

    Ch 1 Lesson 7 complete * Added verbose mode arg to hide/show debug info respectively

--- AIAgent/main.py
+++ AIAgent/main.py
@@ -8,6 +8,7 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="AIAgent")
     parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     
     load_dotenv()
@@ -17,9 +18,12 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client, messages)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+        
+    generate_content(client, messages, args.verbose)
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose=False):
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages
@@ -27,8 +31,10 @@ def generate_content(client, messages):
     if response.usage_metadata == None:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    if verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    
     print(f"Response:\n{response.text}")
     
 if __name__ == "__main__":
