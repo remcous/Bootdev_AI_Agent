732070e1fda431b6ed87c5ad2ddfe2f8d2e4ace4
remcous <67499364+remcous@users.noreply.github.com>
2026-04-06 22:00:15 -0700

    Chapter 1 Lessons 4 & 5 complete * prints token usage metadata * takes command line argument for prompt

--- AIAgent/main.py
+++ AIAgent/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,11 +9,22 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("GEMINI_API_KEY environment variable not found")
 
+parser = argparse.ArgumentParser(description="AIAgent")
+parser.add_argument("user_prompt", type=str, help="User Prompt")
+
+args = parser.parse_args()
+
 client = genai.Client(api_key=api_key)
+
+print(f"User Prompt: {args.user_prompt}")
 
 response = client.models.generate_content(
     model='gemini-2.5-flash', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    contents=args.user_prompt
 )
 
-print(response.text)
+if response.usage_metadata == None:
+    raise RuntimeError("API request failed, recieved response with no usage metadata")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response:\n{response.text}")
